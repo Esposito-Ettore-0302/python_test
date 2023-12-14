df8c4f3387b9327e92c2851d149390445a0d92c2
Esposito-Ettore-0302 <ettore.esposito.0302@gmail.com>
2023-12-14 11:40:51 +0100

    multithread inefficiency

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 from math import sqrt
 from time import sleep
+import threading, multiprocessing
 
-n = 0
-while True:
-    n += 1
-    prime = True
-    for i in range(2, int(sqrt(n)) + 1):
-        if n % i == 0:
-            prime = False
-            break
-    if prime:
-        print(n)
+def primeS():
+    n = 0
+    while True:
+        n += 1
+        prime = True
+        for i in range(2, int(sqrt(n)) + 1):
+            if n % i == 0:
+                prime = False
+                break
+        if prime:
+            print(n)
+
+processes = multiprocessing.cpu_count() * 4
+for i in range(processes):
+    p = threading.Thread(target=primeS)
+    p.start()
+block = input()
