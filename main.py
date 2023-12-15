784418f09aaf7e4bc82b93814e54435687b2cf82
Esposito-Ettore-0302 <ettore.esposito.0302@gmail.com>
2023-12-15 11:55:03 +0100

    very inefficient multiprocessing

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from math import sqrt
 from time import sleep
 import threading, multiprocessing
 
-def primeS():
+def prime_thread():
     n = 0
     while True:
         n += 1
@@ -14,8 +14,20 @@ def primeS():
         if prime:
             print(n)
 
-processes = multiprocessing.cpu_count() * 4
-for i in range(processes):
-    p = threading.Thread(target=primeS)
-    p.start()
-block = input()
+def prime_process():
+    thread_count = multiprocessing.cpu_count() * 4
+    for I in range(thread_count):
+        th = threading.Thread(target=prime_thread)
+        th.start()
+    block = input()
+
+if __name__ == "__main__":
+    processes_count = multiprocessing.cpu_count() * 4
+    processes = []
+    for i in range(processes_count):
+        p = multiprocessing.Process(target=prime_process)
+        processes.append(p)
+        p.start()
+
+    for p in processes:
+        p.join()
